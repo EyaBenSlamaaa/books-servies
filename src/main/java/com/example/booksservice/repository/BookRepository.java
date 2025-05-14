6f00babe8aed18cdbc081545716d7cc95900cd39
EyaBenSlamaaa <eya.benslama.eya@gmail.com>
2025-05-14 11:34:56 +0100

    Version 3: Swagger and PostgreSQL

--- src/main/java/com/example/booksservice/repository/BookRepository.java
+++ src/main/java/com/example/booksservice/repository/BookRepository.java
@@ -2,6 +2,12 @@ package com.example.booksservice.repository;
 
 import com.example.booksservice.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, String> {
+    List<Book> findByAuthor(String author);
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    boolean existsByIsbn(String isbn);
 }
